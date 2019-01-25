11ed2de12b24a92da69e95021ddd05605b9970c4
Ledson Silva <ledson.dev@gmail.com>
2019-01-24 23:10:19 -0200

    Build project with docker.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  weather_api: 'http://api.openweathermap.org/data/2.5/',
+  weather_api_key: 'e071cd24bb755f14454c37ebf2383a43'
 };
